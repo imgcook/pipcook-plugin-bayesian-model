dffe4b95453abbaea76c1f007b8fbba63c426da3
FeelyChau <feely@outlook.com>
2021-03-08 16:16:13 +0800

    implement pipeline 2.0

--- src/index.ts
+++ src/index.ts
@@ -2,52 +2,86 @@
  * @file This is for the plugin to load Bayes Classifier model.
  */
 
-import { ModelDefineType, UniModel, ModelDefineArgsType, CsvDataset, CsvSample } from '@pipcook/pipcook-core';
+import { UniModel, CsvDataset } from '@pipcook/pipcook-core';
 import * as assert from 'assert';
 import * as path from 'path';
-import { processPredictData, getBayesModel, loadModel } from './script';
-
-const boa = require('@pipcook/boa');
-const sys = boa.import('sys');
-
-/**
- * assertion test
- * @param data
- */
-const assertionTest = (data: CsvDataset) => {
-  assert.ok(data.metadata.feature && data.metadata.feature.names.length === 1,
-    'feature should only have one dimension which is the feature name');
-};
+import * as fs from 'fs-extra';
+import {
+  getBayesModel,
+  loadModel,
+  TextProcessing,
+  MakeWordsSet,
+  words_dict,
+  TextFeatures,
+  save_all_words_list,
+  saveBayesModel
+} from './script';
+import { cn, en } from './stopwords';
 
 /**
  * Pipcook Plugin: bayes classifier model
  * @param data Pipcook uniform sample data
  * @param args args. If the model path is provided, it will restore the model previously saved
  */
-const bayesianClassifierModelDefine: ModelDefineType = async (data: CsvDataset, args: ModelDefineArgsType): Promise<UniModel> => {
+const modelDefine = async (options: Record<string, any>, api: any): Promise<any> => {
+  const { boa } = api;
+  const sys = boa.import('sys');
   const {
     recoverPath
-  } = args;
-
+  } = options;
+      
   sys.path.insert(0, path.join(__dirname, 'assets'));
   let classifier: any;
 
   if (!recoverPath) {
-    assertionTest(data);
-    classifier = getBayesModel();
+    // assertionTest(data);
+    classifier = getBayesModel(boa);
   } else {
-    classifier = await loadModel(path.join(recoverPath, 'model.pkl'));
+    classifier = await loadModel(path.join(recoverPath, 'model.pkl'), boa);
   }
-
-  const pipcookModel: UniModel = {
-    model: classifier,
-    predict: async function (text: CsvSample) {
-      const processData = await processPredictData(text.data, path.join(recoverPath, 'feature_words.pkl'), path.join(recoverPath, 'stopwords.txt'));
-      const pred = this.model.predict(processData);
-      return pred.toString();
-    }
-  };
-  return pipcookModel;
+  return classifier;
 };
 
-export default bayesianClassifierModelDefine;
+/**
+ *
+ * @param data Pipcook uniform data
+ * @param model Eshcer model
+ */
+const modelTrain = async (option: Record<string, any>, api: any, model: any): Promise<UniModel> => {
+  const {
+    modelPath,
+    mode = 'cn'
+  } = option;
+  const { boa } = api;
+  const sys = boa.import('sys');
+
+  sys.path.insert(0, path.join(__dirname, 'assets'));
+
+  const classifier = model;
+
+  const rawData = [];
+  const rawClass = [];
+  let sample = await api.dataSource.nextTrain();
+  while (sample) {
+    rawData.push(sample.data);
+    rawClass.push(sample.label);
+    sample = await api.dataSource.nextTrain();
+  };
+  const text_list = TextProcessing(rawData, rawClass, boa);
+
+  let stopWords = mode === 'en' ? en : cn;
+  const stopwords_set = await MakeWordsSet(stopWords);
+  const feature_words = words_dict(text_list[0], stopwords_set);
+  const feature_list = TextFeatures(text_list[1], feature_words, boa);
+  classifier.fit(feature_list, text_list[2]);
+  await fs.writeFile(path.join(modelPath, 'stopwords.txt'), stopWords);
+  save_all_words_list(feature_words, path.join(modelPath, 'feature_words.pkl'), boa);
+  saveBayesModel(classifier, path.join(modelPath, 'model.pkl'), boa);
+  return classifier;
+};
+
+const main = async(options: Record<string, any>, api: any) => {
+  let model = await modelDefine(options, api);
+  model = await modelTrain(options, api, model);
+};
+export default main;
--- src/script.ts
+++ src/script.ts
@@ -2,14 +2,7 @@
 
 import fs from 'fs';
 import readline from 'readline';
-
-const boa = require('@pipcook/boa');
-
-const jieba = boa.import('jieba');
-const { MultinomialNB } = boa.import('sklearn.naive_bayes');
-
-const pickle = boa.import('pickle');
-const { open, list } = boa.builtins();
+import { zip, unzip } from 'lodash';
 
 function strip(str: string): string {
   return str.replace(/(^\s*)|(\s*$)/g, '');
@@ -19,26 +12,19 @@ function isdigit(str: string): boolean {
   return /^\d+$/.test(str);
 }
 
-function MakeWordsSet(words_file: string): Promise<Set<string>> {
-  const words_set = new Set<string>();
-  const rl = readline.createInterface({
-    input: fs.createReadStream(words_file)
-  });
-  return new Promise((resolve) => {
-    rl.on('line', (line: string) => {
-      const word = strip(line);
-      if (word.length > 0 && !words_set.has(word)) {
-        words_set.add(word);
-      }
-    });
-
-    rl.on('close', () => {
-      resolve(words_set);
-    });
-  });
+export function MakeWordsSet(allWords: string): Set<string> {
+  const wordsSet = new Set<string>();
+  let wordsList = allWords.split('\n');
+  for(let i = 0; i < wordsList.length; i++) {
+    const word = strip(wordsList[i]);
+    if (word.length > 0 && !wordsSet.has(word)) {
+      wordsSet.add(word);
+    }
+  }
+  return wordsSet;
 }
 
-function words_dict(all_words_list: string[], stopwords_set = new Set<string>()): string[] {
+export function words_dict(all_words_list: string[], stopwords_set = new Set<string>()): string[] {
   const feature_words: string[] = [];
   for (let word of all_words_list) {
     if (!isdigit(word) &&
@@ -52,36 +38,84 @@ function words_dict(all_words_list: string[], stopwords_set = new Set<string>())
   return feature_words;
 }
 
-export const processPredictData = async function (data: any, all_words_list_path: string, stopwords_path: string): Promise<number[][]> {
-  const all_words_list = pickle.load(open(all_words_list_path, 'rb'));
-  const word_cut = jieba.cut(data, boa.kwargs({
-    cut_all: false
-  }));
+export const getBayesModel = function (boa: any) {
+  const { MultinomialNB } = boa.import('sklearn.naive_bayes');
+  return MultinomialNB();
+};
 
-  const stopwords_file = stopwords_path;
-  const text_features = function (text: string, feature_words: string[]): number[] {
-    const text_words = new Set(text);
-    const features = [];
-    for (let word of feature_words) {
-      if (text_words.has(word)) {
-        features.push(1);
+export const loadModel = async function (filepath: string, boa: any) {
+  const pickle = boa.import('pickle');
+  const { open } = boa.builtins();
+  return boa.with(open(filepath, 'rb'), pickle.load);
+};
+interface AllWordsDict {
+  [key: string]: [ string, number ];
+}
+export const TextProcessing = function(row_data: string[], row_class: string[], boa: any): any[][] {
+  const jieba = boa.import('jieba');
+  const random = boa.import('random');
+
+  const { list } = boa.builtins();
+
+  const data_list: string[][] = [];
+  const class_list: string[] = [];
+
+  row_data.forEach((data, i) => {
+    const word_cut: string[] = jieba.cut(data, boa.kwargs({
+      cut_all: false
+    }));
+    data_list.push(list(word_cut));
+    class_list.push(row_class[i]);
+  });
+  // split train set and testing set
+  const data_class_list = zip(data_list, class_list);
+  random.shuffle(data_class_list);
+  const [ train_data_list, train_class_list ] = unzip(data_class_list);
+
+  const all_words_dict: AllWordsDict = {};
+  for (const word_list of train_data_list) {
+    for (const word of word_list) {
+      if (!all_words_dict[word]) {
+        all_words_dict[word] = [ word, 1 ];
       } else {
-        features.push(0);
+        all_words_dict[word][1] += 1;
       }
     }
+  }
 
-    return features;
-  };
+  const all_words_tuple_list = Object.values(all_words_dict).sort((item1, item2) => item2[1] - item1[1]);
+  const all_words_list = unzip(all_words_tuple_list)[0];
 
-  const stopwords_set = await MakeWordsSet(stopwords_file);
-  const feature_words = words_dict(all_words_list, stopwords_set);
-  return [ text_features(list(word_cut), feature_words) ];
+  return [ all_words_list, train_data_list, train_class_list ];
 };
 
-export const getBayesModel = function () {
-  return pickle.dumps(MultinomialNB());
+function text_features(text: string, feature_words: string[], boa: any) {
+  const { set } = boa.builtins();
+  const text_words = set(text);
+  return boa.eval`[1 if word in ${text_words} else 0 for word in ${feature_words}]`;
+}
+
+export const TextFeatures = function(train_data_list: string[], feature_words: string[], boa: any) {
+  return train_data_list.map((text: string) => {
+    return text_features(text, feature_words, boa);
+  });
 };
 
-export const loadModel = async function (filepath: string) {
-  return boa.with(open(filepath, 'rb'), pickle.load);
+export const save_all_words_list = function(feature_words: any, filepath: string, boa: any) {
+  const pickle = boa.import('pickle');
+
+  const { open } = boa.builtins();
+
+  return boa.with(open(filepath, 'wb'), (f: any) => {
+    return pickle.dump(feature_words, f);
+  });
+};
+
+export const saveBayesModel = async function(classifier: any, filepath: string, boa: any) {
+  const pickle = boa.import('pickle');
+
+  const { open } = boa.builtins();
+  return boa.with(open(filepath, 'wb'), (f: any) => {
+    return pickle.dump(classifier, f);
+  });
 };
